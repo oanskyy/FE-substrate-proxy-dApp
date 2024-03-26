1a47e6e528bbbd797c3322aa2ad2b67c3f9ca759
oanskyy <oana.anghelachi@gmail.com>
2024-03-26 01:03:38 -0300

    style(wallet): add wallet sidebar slider

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import WalletConnectComponent from "./components/WalletConnectComponent"
 function App() {
 	return (
 		<div className='p-10'>
-			<header className=''>
-				<h2 className='text-2xl md:text-3xl font-bold'>Substrate Frontend</h2>
-				<WalletConnectComponent />
+			<header className='flex flex-col items-center md:items-start md:flex-row md:justify-between gap-1 mt-5 mb-2'>
+				<div className=''>
+					<h2 className='text-2xl md:text-3xl font-bold'>Substrate Frontend</h2>
+				</div>
+				<div className='flex flex-col md:justify-self-end md:content-end md:items-end md:self-end mt-4 md:mt-0'>
+					<WalletConnectComponent />
+				</div>
 			</header>
 		</div>
 	)
--- src/components/WalletConnectComponent.tsx
+++ src/components/WalletConnectComponent.tsx
@@ -4,6 +4,7 @@ import { connectWallet } from "../utils/walletConnection"
 const WalletConnectComponent: React.FC = () => {
 	const [connectedAddresses, setConnectedAddresses] = useState<string[]>([])
 	const [selectedAddress, setSelectedAddress] = useState<string | null>(null)
+	const [showSidebar, setShowSidebar] = useState(false)
 
 	const handleConnectWallet = async () => {
 		try {
@@ -18,33 +19,60 @@ const WalletConnectComponent: React.FC = () => {
 		setSelectedAddress(address)
 	}
 
+	const toggleSidebarAndConnectWallet = () => {
+		handleConnectWallet()
+		setShowSidebar(!showSidebar)
+	}
+
 	return (
-		<div>
-			<button
-				onClick={handleConnectWallet}
-				className='rounded-full bg-talisman-green text-talisman-grey-2 text-sm px-2 py-0.5'
-			>
-				Connect Wallet
-			</button>
-			{connectedAddresses.length > 0 && (
-				<div>
-					<p>Connected addresses:</p>
-					<ul>
-						{connectedAddresses.map((address, index) => (
-							<li
-								key={index}
-								className={`cursor-pointer ${
-									selectedAddress === address ? "font-bold" : ""
-								}`}
-								onClick={() => handleAddressSelect(address)}
-							>
-								{`${address.slice(0, 8)}...${address.slice(-8)}`}
-							</li>
-						))}
-					</ul>
-				</div>
+		<>
+			{showSidebar ? (
+				<button
+					className='flex text-xl items-center cursor-pointer fixed right-8 top-4 z-50'
+					onClick={() => setShowSidebar(!showSidebar)}
+				>
+					x
+				</button>
+			) : (
+				<button
+					onClick={toggleSidebarAndConnectWallet}
+					className='z-30 flex cursor-pointer self-center md:self-end w-fit text-sm rounded-full bg-talisman-green hover:opacity-75 ease-in-out text-talisman-grey-2 px-5 py-2'
+				>
+					Connect Wallet
+				</button>
 			)}
-		</div>
+			<div className='flex flex-col'>
+				<div
+					className={`top-0 right-0 w-full md:w-min lg:w-[32vw] p-8 bg-talisman-sidebar text-white fixed h-full z-40 ease-in-out duration-300 ${
+						showSidebar ? "translate-x-0 " : "translate-x-full"
+					}`}
+				>
+					{connectedAddresses.length > 0 && (
+						<div className='mt-4 '>
+							<h4 className='text-2xl lg:text-3xl font-semibold'>
+								Connected addresses
+							</h4>
+							<p className='text-sm mt-4 text-talisman-grey-1'>
+								Select the address
+							</p>
+							<ul>
+								{connectedAddresses.map((address, index) => (
+									<li
+										key={index}
+										className={`cursor-pointer text-md px-3.5 py-3 rounded-lg mt-2 bg-talisman-li ${
+											selectedAddress === address ? "font-bold" : ""
+										}`}
+										onClick={() => handleAddressSelect(address)}
+									>
+										{`${address.slice(0, 9)}...${address.slice(-9)}`}
+									</li>
+								))}
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
+		</>
 	)
 }
 
